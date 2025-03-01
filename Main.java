6f274699782ce29ad501a5d523124a029d28c851
mariam baramidze <161630246+mariamo123M@users.noreply.github.com>
2025-03-01 23:33:12 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static double averangeOfFourNumbers(int a, int b, int c, int d) {
+        return (a + b + c + d) / 4.0; //
+    }
+
+    public static void main(String[] args) {
+        System.out.println("averange:" + averangeOfFourNumbers(8, 12, 20, 32));
+    }
+}
