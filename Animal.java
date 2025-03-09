379a67ea22ab6e4fa757c1423420dee1ef2448b4
mariam baramidze <161630246+mariamo123M@users.noreply.github.com>
2025-03-09 23:40:24 +0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    public static void main(String[] args) {
+        String animalName = "horse";
+        if (animalName.equals("horse")) {
+            System.out.println(" I am an animal");
+        } else if (animalName.equals("lion")) {
+            System.out.println("I am the lion");
+        } else if (animalName.equals("tiger")) {
+            System.out.println("I am an animal");
+        } else {
+            System.out.println("i am an animal");
+        }
+    }
+}
