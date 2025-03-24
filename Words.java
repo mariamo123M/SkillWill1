26d8454dd6079ec60888e2650a05de5945210a1e
mariam baramidze <161630246+mariamo123M@users.noreply.github.com>
2025-03-24 10:47:26 +0400

    Add files via upload

---
+++ Words.java
@@ -0,0 +1,9 @@
+class Main {
+        public static void main(String[] args) {
+                String[] words = {"Bakhmaro", "Horse", "Star", "Sun", "Moon", "Sunrise", "Lucky", "Mountain", "Sky", "Sea"};
+                for (int i = 0; i < words.length; i++) {
+                        System.out.println(words[i]);
+
+                }
+        }
+}
