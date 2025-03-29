ec382444faac3593983c58f3316f9d5705791548
mariam baramidze <161630246+mariamo123M@users.noreply.github.com>
2025-03-29 14:13:07 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int a = 2;
-        int b = 3;
-        int d = 1;
-        int i = 1;
+        ArrayList<String> words = new ArrayList<>();
 
-        while (i < b){
-            i ++;
-            d *= a;
+        words.add("Love");
+        words.add("Horse");
+        words.add("Lucky");
+        words.add("Moon");
+        words.add("Dog");
+        words.add("Goat");
+        words.add("Happy");
+        words.add("sun");
+        words.add("mountain");
+        words.add("Star");
+
+        for (int i = 0; i<words.size(); i++) {
+            System.out.println(words.get(i));
+
         }
-            System.out.println("while:" + d);
     }
 }
