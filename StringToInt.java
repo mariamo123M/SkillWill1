86e5d97bdf3b9b54b2688ddd12a319dd8c2f0a00
mariam baramidze <161630246+mariamo123M@users.noreply.github.com>
2025-04-05 14:56:38 +0400

    Add files via upload

---
+++ StringToInt.java
@@ -0,0 +1,17 @@
+
+public class StringToInt {
+
+    public static int convert(String numberAsString) {
+        return Integer.parseInt(numberAsString);
+    }
+
+    public static void main(String[] args) {
+        int number = convert("77");
+        System.out.println("Result: " + convert("77"));
+    }
+}
+
+
+
+
+
