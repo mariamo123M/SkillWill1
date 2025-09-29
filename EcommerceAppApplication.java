0dbb203ef0646423f68b44a6241a9648c178b375
mariam baramidze <161630246+mariamo123M@users.noreply.github.com>
2025-09-29 21:01:46 +0400

    Add files via upload

---
+++ EcommerceAppApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAspectJAutoProxy
+public class EcommerceAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceAppApplication.class, args);
+    }
+}
